70e4560e95aff21cc05e72d7b63f6036b9617145
jerrydev <hoptran270399@gmail.com>
2024-03-21 02:00:34 +0700

    add

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-const express = require('express')
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
+dbConnect();
 
-app.use(express.json) // đọc data client gửi lên theo kiểu json
-app.use(express.urlencoded({extended: true})) // phân tích dữ liệu dưới dạng application/x-www-form-urlencoded từ các post put
+app.use(express.json); // đọc data client gửi lên theo kiểu json
+app.use(express.urlencoded({ extended: true })); // phân tích dữ liệu dưới dạng application/x-www-form-urlencoded từ các post put
 
-app.use('/', (req, res) => {res.send('server on')})
+app.use("/", (req, res) => {
+  res.send("server on");
+});
 
-app.listen(port, () =>{
-    console.log('start server with port: ' + port);
-})
+app.listen(port, () => {
+  console.log("start server with port: " + port);
+});
